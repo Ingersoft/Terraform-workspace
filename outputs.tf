2a6410f4b45a536c75aa80c4fd23a7afce552d3e
Inger Montenegro <imontenegro@hitachisolutions.com>
2024-02-11 17:51:52 -0600

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,23 @@ output "application_url" {
   value       = "${aws_instance.web.public_dns}/index.php"
 }
 */
+
+output "resource_group_name" {
+  description = "The name of the created resource group."
+  value       = azurerm_resource_group.this.name
+}
+
+output "virtual_network_name" {
+  description = "The name of the created virtual network."
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "subnet_name_1" {
+  description = "The name of the created subnet 1."
+  value       = azurerm_subnet.snet_app.name
+}
+
+output "subnet_name_2" {
+  description = "The name of the created subnet 2."
+  value       = azurerm_subnet.snet_dba.name
+}
